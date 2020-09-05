7b38cb9116f09a7386a38937a4206d53e2e11867
QuangTruong23 <63149001+QuangTruong23@users.noreply.github.com>
2020-09-05 18:00:02 +0700

    add python.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#chương trình chạy thử
+tuoi = 20
+ten = "quang truong"
+PI = 3.14
+print ("tuoi la : " + str(tuoi))
+print ("ten la: " + str (ten))
+print ("so PI la :"+ str(PI))
+print(type(tuoi))
+print(type(ten))
+print(type(PI))
+
